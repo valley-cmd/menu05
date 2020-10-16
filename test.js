b00c1fa4a6a5dfde45c427ff38640b17fe82c9e0
valley nk <n.valley.k@gmail.com>
2020-10-16 17:30:55 +0900

    テンプレ

---
+++ test.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.menu-btn').on('click', function(){
+    $('.menu').toggleClass('is-active');
+    $('.menu-btn').toggleClass('open');
+  });
+}());
+
+// $(function() {
+//   alert('OK!');
+// });
